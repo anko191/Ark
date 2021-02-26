df33c6ebacf0f24d83bcde27b775a016bd6f4bec
anko191 <anko101022@yahoo.co.jp>
2021-02-26 18:47:36 +0900

    --updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+MEDIA_URL = '/uploads/'
 if sw:
     STATIC_ROOT = './static'
     MEDIA_ROOT = './uploads'
